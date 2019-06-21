326d5bc618dcdc6a16a43a672823beb376429680
Jkirby17 <jacquekirby10@gmail.com>
2019-06-21 13:09:44 -0600

    changes

--- web-front-dev/weather/js/functions.js
+++ web-front-dev/weather/js/functions.js
@@ -4,12 +4,8 @@ console.log('My javascript is being read.');
 
 // Calculate the Windchill
 function buildWC(speed, temp) {
-
+    let feelTemp = document.getElementById('feelTemp');
 }
-
-
-
-let feelTemp = document.getElementById('feelTemp');
 
 // Compute the windchill
 let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
