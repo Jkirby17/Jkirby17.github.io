fa9033c5ff2cfef8f2803fc78703d53d3d5f56ab
Jkirby17 <jacquekirby10@gmail.com>
2019-06-26 13:40:24 -0600

    switch

--- web-front-dev/weather/js/localdata.js
+++ web-front-dev/weather/js/localdata.js
@@ -5,25 +5,34 @@ let hourlyList = document.getElementById("hourlyData");
 
 pageNav.addEventListener("click", function (evt) {
     let cityName = evt.target.innerHTML;
+    console.log(cityName);
 
-}) //ends the event listener
-
-let weatherURL = "/greenville-local/js/weather/json";
-
-//fetchData(weatherURL);
-
-//function fetchData(weatherURL) {
-// let cityName = 'Greenville'; // The data we want from the weather.json file
-fetch(weatherURL)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
+    //only check for city names
+    switch (cityName) {
+        case "Franklin":
+        case "Greenville":
+        case "Springfield":
+            evt.preventDefault();
+            break;
+    }
 
 
+    let weatherURL = "/greenville-local/js/weather/json";
+
+    //fetchData(weatherURL);
+
+    //function fetchData(weatherURL) {
+    // let cityName = 'Greenville'; // The data we want from the weather.json file
+    fetch(weatherURL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
 
 
 
-//}..ends function
-// }) // ends the event listener
+
+
+    //}..ends function
+}) // ends the event listener
